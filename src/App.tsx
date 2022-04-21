153e748f017a1ccd2e289d4a1f212c9de4b95d1f
Leonardo Maron <leo22.maron@hotmail.com>
2022-04-21 00:20:37 -0300

    Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { useEffect, useState } from 'react';
 
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
-
-import './styles/content.scss';
 
 interface GenreResponseProps {
   id: number;
@@ -17,27 +13,12 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-  const [movies, setMovies] = useState<MovieProps[]>([]);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
   useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
 
     api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
       setSelectedGenre(response.data);
@@ -55,19 +36,7 @@ export function App() {
         handleClickButton={handleClickButton }
       />
 
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content selectedGenreId={selectedGenreId} />
     </div>
   )
 }
